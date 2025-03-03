e93da8091a8e8f57669f3109975fb77d9e8920b0
Brandon Moriguchi <63900247+MoriguchiBrandon@users.noreply.github.com>
2025-03-03 12:13:53 -0800

    trying to set up dash page

---
+++ app4.py
@@ -0,0 +1,39 @@
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
+
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# Initialize the app
+app = Dash()
+
+# App layout
+app.layout = html.Div([
+    #Title String
+    html.Div(children='My First App with Data, Graph, and Controls'),
+    html.Hr(),
+
+    # Giving optional buttons
+    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
+
+    # Creating the table and graph
+    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
+    dcc.Graph(figure={}, id='controls-and-graph')
+])
+
+# Add controls to build the interaction
+@callback(
+    # Dash will cme back to this function whenever input is updated
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value')
+)
+# The function used to update the graph
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
